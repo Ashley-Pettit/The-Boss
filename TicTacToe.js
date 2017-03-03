c0254983143fae0838cdeb173e1300e4ce3f15df
Ashley-Pettit <Ashley@digital380.com.au>
2017-03-03 17:04:20 +1100

    decreased cheating

--- TicTacToe.js
+++ TicTacToe.js
@@ -417,7 +417,7 @@ function AICheater() {
     playToWin();
     return;
   }
-  else if (app.turn > 7 && canStealCellAndWin() && feelLikeCheating(.15)) {
+  else if (app.turn > 7 && canStealCellAndWin() && feelLikeCheating(.12)) {
     console.log("Turn " + app.turn + ". The computer stole cell " + app.stealWhichCellToWin +  " to win.");
     app.turn++
     stealCell()
@@ -479,7 +479,7 @@ function stealCell(playType) {
 function cheatingMoves() {
   app.currentPlayer = 'X'
   if (((app.turn > 5) && app.turn < 8) && app.isRoundInProgress === true) {
-    if (isComputerAbleToWin() && ((app.turn > 6) && app.turn < 9) && feelLikeCheating(.17)) {
+    if (isComputerAbleToWin() && ((app.turn > 6) && app.turn < 9) && feelLikeCheating(.15)) {
       app.turn++
       playToWin();
       console.log("Turn " + app.turn + ". The computer snuck victory with a dirty double play.");
@@ -487,13 +487,13 @@ function cheatingMoves() {
     }
     else if (doesComputerNeedToBlock()) {
       console.log('%cWARNING - 2 way win detected. Cheating odds have been drastically increased.', 'color: red')
-      if (isComputerAbleToWin() && feelLikeCheating(.80)){
+      if (isComputerAbleToWin() && feelLikeCheating(.75)){
         app.turn++
         playToWin();
         console.log("Turn " + app.turn + ". A sneaky double play was used for an instant win.");
         return;
       }
-      else if (canStealCellAndWin() && feelLikeCheating(.85)) {
+      else if (canStealCellAndWin() && feelLikeCheating(.80)) {
         stealCell();
         console.log("Turn " + app.turn + ". With impending doom the computer had no choice but to steal cell " + app.stealWhichCellToWin +  " to win.");
         return;
@@ -510,7 +510,7 @@ function cheatingMoves() {
 }
 
 function cheatOnDraw() {
-  if (checkForDraw() && app.round > 6 && feelLikeCheating(.35)) {
+  if (checkForDraw() && app.round > 5 && feelLikeCheating(.35)) {
     for (var i = 0; i < 9; i++) {
       gameBoard[i] = 'X';
       $('#' + i).text('X');
